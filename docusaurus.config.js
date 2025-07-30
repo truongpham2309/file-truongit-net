37350056af05985b2325f3da51f2e61a94b9ddb7
truongpham2309 <phamnhattruong2309@gmail.com>
2025-07-31 00:04:07 +0700

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'truongpham2309', // Usually your GitHub org/user name.
-    projectName: 'file-truongit-netset GIT_USER=truongpham2309&& npm run deploy', // Usually your repo name.
+    projectName: 'file-truongit-netset', // Usually your repo name.
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
